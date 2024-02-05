d4f7774e72aadf39bf2a6992c7c37e60375e8ba1
chalfontchubby <rikallen@yahoo.com>
2024-02-05 20:59:02 +0000

    Make the decoder a class, early out of the decode if second byte isn't 0x40
    Restart the loop on 0x2020

--- ibus_decode.py
+++ ibus_decode.py
@@ -2,62 +2,58 @@ from machine import UART
 import struct
 
 
-class ibus:
-    def __init__(self):
+class ibus_interface:
+    def __init__(self, uart):
         self._buffer = bytearray(30)
+        self._uart = uart
 
+    def read(self):
+        byte = 0
+        while True:
+            # We could have seen a 0x20, 0x20 - so the last byte we read could be the start of the packet if the
+            # first 0x20 was part of the previous checksum.
+            if byte != 0x20:
+                byte = self._uart.read(1)
+            # We must start with a 0x20. If we don't get it - start again
+            if byte !=  b'\x20':
+                continue
+            # Then we need a 0x40, else start again
+            # 
+            byte = self._uart.read(1)
+            if byte != b'\x40':
+                continue
 
-def read_ibus(self):
-    def decode_packet():
-        # ibus packet is 32 bytes, but we already read the first 0x20 byte so
-        # we only get 31 here.
-        
-        # (0x20), 0x40
-        # 14*16 bitvalues, little endian
-        # 16 bit checksum equal to 0xffff minus all bytes except the checksum
-        values = [0] * 14
-        byte_data = memoryview(self._buffer)
-        
-        # We can exit early if the first byte of the buffer isn't the 0x40 we expect
-        if byte_data[0] != 0x40:
-            return None
-        
-        # Checksum is 0xffff - every byte except the checksum itself.
-        # We stripped the 0x20 off the front becore we got here, so we start counting doen from 0xffff - 0x20
-        my_cksum = 0xffff - 0x20 - 0x40
-        # Now we have the rest of the bytes, 32 of them less the 0x20 at the start, and the 2 bytes of checksum
-        for i in range(28):
-            my_cksum -= byte_data[i]
+            # We've seen a 0x20 and a 0x40 - now slurp the next 30 bytes ino the buffer and decode those.
+            # We don't process the 0x2040, so have to remember to deal with it
+            self._uart.readinto(self._buffer)
 
-        *values, cksum = struct.unpack("<HHHHHHHHHHHHHHH", self._buffer)
-        #
-        if cksum != my_cksum:
-            print("Fail")
-            return None
-        return values
-
-
-
-    while True:
-        byte = uart.read(1)
-        if byte == 0x20:
-            byte = uart.read(1)
-            if byte == 0x40:
-                # We've seen a 0x20 and a 0x40 - now slurp the next 30 bytes ino the buffer and decode those.
-                # We don't process the 0x2040, so have to remember to deal with it
-                uart.readinto(self._buffer)
-
-                values = decode_packet()
-                if values is not None:
-                    return values
+            # (0x20, 0x40) " @"
+            # 14*16 bitvalues, little endian
+            # 16 bit checksum equal to 0xffff minus all bytes except the checksum
+            values = [0] * 14
+            # Get a view of the memory as 8 bit values we can process more easily for the checksum
+            byte_data = memoryview(self._buffer)
             
+            # Checksum is 0xffff minus every byte except the checksum itself.
+            # We stripped the 0x20, 0x40 off the front befcore we got here, so we start counting doen from 0xffff - 0x20 - 40
+            my_cksum = 0xffff - 0x20 - 0x40
+            # Now we have the rest of the bytes, 32 of them less the 0x20 at the start, and the 2 bytes of checksum
+            for i in range(28):
+                my_cksum -= byte_data[i]
+    
+            *values, cksum = struct.unpack("<HHHHHHHHHHHHHHH", self._buffer)
+            if cksum == my_cksum:
+                return values
+            # 0x20, 0x40 can't appear bit packet. Neither checksum nor data can begin with 0x20 or 0x40
+
 
 # If we run this file on it's own, then it runs the loop below.
 # Otherwise we can just import it and get the function above
 if __name__ == "__main__":
         
     uart = UART(0, 115200, timeout=1000) # uart1 tx-pin 4, rx-pin 5
+    ibus = ibus_interface(uart)
     while True:
-        values = read_ibus()
+        values = ibus.read()
         if values is not None:
             print(" ".join(str(x) for x in values))
