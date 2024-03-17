abeff88afc644eddd522a7db51c488d91f731273
rikallen@yahoo.com <rikallen@yahoo.com>
2024-03-17 13:59:43 +0000

    Bidirectional ibus - with autoflush. Robot can take over

---
+++ ibus.py
@@ -0,0 +1,172 @@
+import serial
+import struct
+import time
+import threading
+
+# Define which channels of the interface do what
+SPEED_CHAN = 1
+TURN_CHAN = 0
+STRAFE_CHAN = 3
+ENABLE_CHAN = 6
+RUN_APP_CHAN = 7
+KNOB_1 = 4
+KNOB_2 = 5
+
+ENABLE_ON = 2000
+ENABLE_OFF = 1000
+
+ZERO_OFFSET = 1500
+
+# Class to read from the uart assuming it is connected to a ibus reciever
+# If that has the enable channel set high, then write the unmodified packet
+# backout - assuming that tx is connected to a pico decoder
+# Else write out a set of values set up from host software.
+
+class Ibus:
+    def __init__(self, uart, callback):
+        # Pre allocate a buffer for the transactions
+        self._out_buffer = bytearray(32)
+        self._in_buffer = bytearray(30)
+
+        self._uart = uart
+        self._local_values = [1500] * 14
+        self._input_values = [1500] * 14
+        self.disable()
+        self._ibus_thread = threading.Thread(target=self.run, daemon=True)
+        self._ibus_thread.start();
+        self._host_running = False
+        self._callback = callback
+        
+        
+    def read(self):
+        byte = 0
+        while True:
+            # We could have seen a 0x20, 0x20 - so the last byte we read could be the start of the packet if the
+            # first 0x20 was part of the previous checksum.
+            if byte != 0x20:
+                byte = self._uart.read(1)
+            # We must start with a 0x20. If we don't get it - start again
+            if byte !=  b'\x20':
+                continue
+            # Then we need a 0x40, else start again
+            # 
+            byte = self._uart.read(1)
+            if byte != b'\x40':
+                continue
+
+            # We've seen a 0x20 and a 0x40 - now slurp the next 30 bytes ino the buffer and decode those.
+            # We don't process the 0x2040, so have to remember to deal with it
+            bytes_read = self._uart.readinto(self._in_buffer)
+            if bytes_read != 30:
+                continue
+
+            # (0x20, 0x40) " @"
+            # 14*16 bitvalues, little endian
+            # 16 bit checksum equal to 0xffff minus all bytes except the checksum
+
+            # Get a view of the memory as 8 bit values we can process more easily for the checksum
+            byte_data = memoryview(self._in_buffer)
+            
+            # Checksum is 0xffff minus every byte except the checksum itself.
+            # We stripped the 0x20, 0x40 off the front before we got here, so we start counting doen from 0xffff - 0x20 - 40
+            my_cksum = 0xffff - 0x20 - 0x40
+            # Now we have the rest of the bytes, 32 of them less the 0x20 at the start, and the 2 bytes of checksum
+            for i in range(28):
+                my_cksum -= byte_data[i]
+
+            *self._input_values, cksum = struct.unpack("<HHHHHHHHHHHHHHH", self._in_buffer)
+            if cksum == my_cksum:
+                return
+            # 0x20, 0x40 can't appear bit packet. Neither checksum nor data can begin with 0x20 or 0x40
+
+    def get_passthrough(self):
+        return self._input_values[ENABLE_CHAN] < 1500
+
+    def passthrough(self):
+        ##self._uart.write(b'\x20\x40')
+        ##self._uart.write(self._in_buffer)
+        pass
+
+    def run(self):
+        self._uart.reset_input_buffer()
+        loops = 0
+        last_time = time.time()
+        while True:
+            pre_read_time = time.time()
+            if self.get_passthrough() > 100:
+                # There are a few packetsinthe input fifo - better jump 
+                print("Uart catchup")
+                self._uart.reset_input_buffer()
+
+            self.read()
+            time_now = time.time()
+            delta_t = time_now - last_time
+            last_time = time_now
+            #print(f"{loops} {self.get_passthrough()} {self._host_running}")
+            loops += 1
+            if self.get_passthrough():
+                self.passthrough()
+                if self._host_running:
+                    self._host_running = False
+                    self._callback(self._host_running)
+            else:
+                if not self._host_running:
+                    self._host_running = True
+                    self._callback(self._host_running)
+                self.write(self._local_values)
+            # ibus runs at about 7ms period - we can sleep most of that
+            #ime.sleep(0.005)
+    
+    def set_cksum(self):
+        # Calculate and insert the checksum into the last 32 bytes
+        byte_data = memoryview(self._out_buffer)
+        # Checksum is 0xffff minus the sum of the first 30 bytes
+        # (i.e. upto but excluding the checksum).
+        cksum = 0xffff - sum( byte_data[:30] )
+        if cksum < 0:
+            print(f"Error: {Cksum=} out of range") 
+        byte_data[30] = cksum & 0xff
+        byte_data[31] = (cksum >> 8) & 0xff
+
+    def enable(self):
+        self._local_values[ENABLE_CHAN] = ENABLE_ON
+        
+    def disable(self):
+        self._local_values[ENABLE_CHAN] = ENABLE_OFF
+
+    def write(self, values):
+        assert len(values) <= 14
+        if min(values) < 1000 or max(values) > 2000:
+            print(f"Error: Values must be in the range 1000 to 2000")
+            return False
+        # Put the 0x20, 0x40 prefix and the 14 shorts being transmitted into the packet
+        struct.pack_into("<BBHHHHHHHHHHHHHH", self._out_buffer, 0, 0x20, 0x40, *values)
+        self.set_cksum()
+        self._uart.write(self._out_buffer)
+        return True
+
+    # Update the motion parameters if not in passthrough
+    # Values are in the range +/- 500
+    def set_motion(self, speed, turn, strafe):
+        self._local_values[SPEED_CHAN] = speed + ZERO_OFFSET
+        self._local_values[TURN_CHAN] = turn +ZERO_OFFSET
+        self._local_values[STRAFE_CHAN] = strafe +ZERO_OFFSET
+
+def callback(in_control):
+    print(f"{in_control=}")
+
+# If we run this file on it's own, then it runs the loop below.
+# Otherwise we can just import it and get the function above
+if __name__ == "__main__":
+    
+    uart = serial.Serial("/dev/ttyS0", baudrate=115200, timeout=3.0)
+    ibus = Ibus(uart, callback)
+    time.sleep(30)
+#    while True:
+#        for turn in range(-500,500):
+#            ibus.set_motion(0,turn,0)
+#            time.sleep(1)
+        
+        
+        
+    
--- robot.py
+++ robot.py
@@ -1,4 +1,4 @@
-from ibus_encode import ibus_encode
+import ibus
 import serial
 import time
 import threading
@@ -6,75 +6,89 @@ import threading
 from adafruit_servokit import ServoKit
 from mpu6050 import mpu6050
 
-
 class Sensors:
     def __init__(self):
         self._mpu = mpu6050(0x68)
         ## Don't want very fast response - set mpu bandwidth to 10 Hz
         self._mpu.set_filter_range(mpu6050.FILTER_BW_10)
         self._bias_samples = 200  # number of samples to measure to get the initial gyro bias
-        self._time_step = 1
-
+        self._time_step = 0.01
+        # Flag to indicate we are not moving, so any sensor data can be used to work out
+        # bias and drift etc.
+        self._stationary = False
+        
         self._done = False
-        self.calc_bias()
-        self._run_mpu()
         self._gyro_bias = {
             'x': 0.0,
             'y': 0.0,
             'z': 0.0,
         }
-        self._gyro_sum = {
+        self._gyro_intergral = {
             'x': 0.0,
             'y': 0.0,
             'z': 0.0,
         }
-
-    def calc_bias(self):
+        self.calc_mpu_bias()
+        self._mpu_thread = threading.Thread(target=self._mpu_daemon, daemon=True)
+        self._mpu_thread.start();
+        
+    def calc_mpu_bias(self):
         for i in range(self._bias_samples):
             gyro_data = self._mpu.get_gyro_data()
             for axis, val in gyro_data.items():
                 self._gyro_bias[axis] += val / self._bias_samples
 
-    def _run_mpu(self):
+    def _mpu_daemon(self):
         self._last_time = time.time()
+        sleep_time = self._time_step
         while True:
-            time.sleep(self._increment)
+            if sleep_time > 0:
+                time.sleep(sleep_time)
             time_now = time.time()
             time_delta = time_now - self._last_time
             gyro_data = self._mpu.get_gyro_data()
-            for axis in gyro_data.keys():
-                gyro_data[axis] -= self._gyro_bias[axis]
-                self._gyro_sum[axis] += self._gyro_data[axis] * time_delta
-
+            if self._stationary:
+                for axis, val in gyro_data.items():
+                    # low pass filter the bias
+                    self._gyro_bias[axis] = (self._gyro_bias[axis] *
+                        ( self._bias_samples - 1) + gyro_data[axis]) / self._bias_samples
+                    gyro_data[axis] -= self._gyro_bias[axis]
+            else: 
+                for axis in gyro_data.keys():
+                    gyro_data[axis] -= self._gyro_bias[axis]
+                    self._gyro_intergral[axis] += gyro_data[axis] * time_delta
+            self._last_time = time_now
+            # Processing all this took some time - so the sleep should be a touch shorted to account for it
+            sleep_time = self._last_time + self._time_step - time.time()
+            
+    def get_heading(self):
+        return self._gyro_intergral["z"]
 
 class Robot:
     def __init__(self):
         self._uart = serial.Serial("/dev/ttyS0", baudrate=115200, timeout=3.0)
-        self._ibus = ibus_encode(self._uart)
+        self._in_control = False
+        self._ibus = ibus.Ibus(self._uart, self._set_in_control)
         self._sensors = Sensors()
+        
+        self._pid_thread = threading.Thread(target=self._pid_daemon, daemon=True)
+        self._servos = ServoKit(channels=16)
+        
+        self.reset_state()
 
+    def reset_state(self):
+        self._target_state = {
+            "speed": None,
+            "turn": None,
+            "strafe": None,
+            "heading": None
+        }
         # Current state
         self._state = {
             "speed": 0.0,
             "turn": 0.0,
             "strafe": 0.0,
         }
-        # target state
-        self._target_state = {
-            "speed": None,
-            "turn": None,
-            "strafe": None,
-        }
-        # Position in a frame of reference based on starting point and heading
-        # X forwards
-        # Y Right
-        # Orientation in degrees clockwise
-        self._global_pos = {
-            "x": 0.0,
-            "y": 0.0,
-            "orientation": 0.0,
-        }
-        self._servos = ServoKit(channels=16)
 
     def enable_motors(self):
         self._ibus.enable()
@@ -83,12 +97,23 @@ class Robot:
         self._ibus.diable()
 
     def get_heading(self):
-        return self._sensor.get_heading)
+        return self._sensors.get_heading()
 
-        def turn_to_heading(self, new_heading):
-            self._target_heading = new_heading
+    def turn_to_heading(self, new_heading):
+        self._target_heading = new_heading
+        
+    def _set_in_control(self, in_control):
+        self._in_control = in_control
+        if in_control:
+            # Clear any targets when we take over
+            self.reset_state()
+        print(f"{in_control=}")
 
-    if __name__ == "__main__":
-        robot = Robot()
-        while True:
-            print(f"Heading = {robot.get_heading()}")
+    def _pid_daemon(self):
+        pass
+    
+if __name__ == "__main__":
+    robot = Robot()
+    while True:
+        print(f"Heading = {robot.get_heading()}")
+        time.sleep(1)
