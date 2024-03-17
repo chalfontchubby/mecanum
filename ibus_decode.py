518b59c074ab963699f1420a48dfb3221b71403d
rikallen@yahoo.com <rikallen@yahoo.com>
2024-03-17 13:58:47 +0000

    Remove encoder from ibus_decode

--- ibus_decode.py
+++ ibus_decode.py
@@ -2,27 +2,7 @@ from machine import UART
 import struct
 
 
-class ibus_encode:
-    def __init__(self, uart):
-        self._buffer = bytearray(32)
-        self._uart = uart
 
-    def calc_cksum(self):
-        byte_data = memoryview(self._buffer)
-        # Checksum is 0xffff minus every byte except the checksum itself.
-        cksum = 0xffff
-        # Now we have the rest of the bytes, 32 of them less the 0x20 at the start, and the 2 bytes of checksum
-        for i in range(30):
-            cksum -= byte_data[i]
-        byte_data[30] = cksum & 0xff
-        byte_data[31] = (cksum >> 8) & 0xff
-
-    def write(self, values):
-        assert len(values) <= 14
-        struct.pack_into("<HHHHHHHHHHHHHHH", self.buffer, 0, 0x4020, *values)
-        calc_cksum()
-
-=
 
 class ibus_interface:
     def __init__(self, uart):
